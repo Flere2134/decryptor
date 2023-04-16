e99827c7cc2204cf1b9b089eec4cdebba5d4d2eb
Flere21 <aaronhans2134@gmail.com>
2023-04-16 12:28:11 +0800

    fixed infinite loop in lines 26 to 29 by fixing try again function

--- decryptor.py
+++ decryptor.py
@@ -1,26 +1,21 @@
 #try again function
-def try_again():
-    again = None
-    while again is None:
+def try_again(again):
+    while True:
         answer = input('Do you want to use the DECRYPTOR again? ')
         if answer in ["Y", "YES", "yes", "Yes", "y"]:
-            again = True
-            continue
+            return True
         elif answer in ["N", "NO", "no", "No", "n"]:
-            again = False
             print('Thank you for using the decryptor')
-            break
+            return False
         else:
             print('Try answering either YES or NO')
 
 #decrypt function
-def decryptor(e_text):
-#replaces each character with a specific character
-    d_text = e_text.replace('*', 'a').replace('&', 'e').replace('#', 'i').replace('+', 'o').replace('!', 'u')
+def decryptor():
 #asks user input of encrypted text
     e_text = input('Write the encrypted text you want to decrypt: ')
-#program decrypts the encrypted text
-    d_text = decryptor()
+#replaces each character with a specific character
+    d_text = e_text.replace('*', 'a').replace('&', 'e').replace('#', 'i').replace('+', 'o').replace('!', 'u')
 #prints decrypted text
     print(f'The message from the encrypted text: {d_text}')
 #returns decrypted text
@@ -28,8 +23,7 @@ def decryptor(e_text):
 
 #sample input: th& q!#ck br+wn f+x j!mps +v&r th& l*zy d+g
 
-while True:
-    decryptor()
-    try_again()
-    if not again:
-        break
+again = True
+while again:
+    d_text = decryptor()
+    again = try_again(again)
