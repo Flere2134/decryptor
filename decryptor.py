37a02f816a9e257cee14903ed27def69ce40c0f9
Flere21 <aaronhans2134@gmail.com>
2023-04-16 11:36:46 +0800

    fixed try again function, not actually working previously

--- decryptor.py
+++ decryptor.py
@@ -3,12 +3,14 @@ def try_again():
     again = None
     while again is None:
         answer = input('Do you want to use the DECRYPTOR again? ')
-        if answer == "Y" or "YES" or "yes" or "Yes":
+        if answer == "Y" or answer == "YES" or answer == "yes" or answer == "Yes" or answer == "y":
             again = str(answer)
             continue
-        elif answer == "N" or "NO" or "no" or "No":
+        elif answer == "N" or answer == "NO" or answer == "no" or answer == "No" or answer == "n":
             again = str(answer)
             print('Thank you for using the decryptor')
             break
         else:
             print('Try answering either YES or NO')
+
+try_again()
